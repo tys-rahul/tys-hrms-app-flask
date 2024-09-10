47c0d3acc8cc36446f2f954d1cca39e9e22b62e5
Rahul Singh <104415697+tys-rahul@users.noreply.github.com>
2024-09-10 10:18:31 +0530

    [Feature] - Added gunicorn to requirements file & procfile & added home route

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from app.extensions import db, jwt, migrate
 from app.config import Config
 from app.routes.main import main_blueprint
@@ -6,6 +6,10 @@ from app.routes.main import main_blueprint
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    
+    @app.route('/')
+    def home():
+        return jsonify({"message": "Flask API is running!"})
 
     db.init_app(app)
     jwt.init_app(app)
